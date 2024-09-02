5fd6d555dce68d54e9c58b6abed871f5247500a6
Anas Kaarov <anasbinkurban@gmail.com>
2024-09-02 22:02:19 +0600

    feat: Разметка сообщений | Настройки Pydantic | Телеграм бот на Python с нуля на aiogram

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
 import os
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+    )
+
+    bot_token: str
+
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+settings = Settings(bot_token=BOT_TOKEN)
